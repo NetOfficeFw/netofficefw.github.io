f6278a4b581ae41abe2197734b069d5e2b37e7e1
Jozef Izso <jozef.izso@gmail.com>
2014-11-06 03:10:45 +0100

    Enhanced cloning of the gh-pages branch

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -4,14 +4,12 @@ shopt -s extglob
 
 # clone gh-pages branch to the output directory
 mkdir out
-
-echo "Cloning current repository to output directory"
-git clone . out
-
 cd out
 
-echo "Checking out branch $GHPAGES_BRANCH for Github Pages"
-git checkout $GHPAGES_BRANCH
+
+echo "Checking out Github Pages branch '$GHPAGES_BRANCH' to output directory"
+git clone -b $GHPAGES_BRANCH --single-branch ../
+
 rm -rf !(.git)
 
 cd ..
